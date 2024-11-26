fbaa87a036ae704c14d70813b6dca462a4a92be7
seyaduraja <seyadusha33@gmail.com>
2024-11-26 06:01:52 +0530

    fixed alignment

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -9,21 +9,21 @@ function Detail() {
     <div>
 
     <div  className='container1'>
-         <img src= {player} alt="" className=' h-[300px] w-[600px]' />
-         <div className='flex flex-col gap-[10px] w-[400px]   md:w-[650px] '>
-         <h2 className='text-[30px] md:text-[50px] lg:text-[60px]  text-white font-semibold  tracking-wide'>Why Choose VeBook?</h2>
-         <p className='text-[18px] text-white text-justify font-semibold tracking-wide '>Discover a wide range of sports venues at your fingertips with a hassle-free booking process, 
+         <img src= {player} alt="" className=' h-[200px] w-[350px] md:h-[300px] md:w-[600px] pr-[20px] md:pr-0' />
+         <div className='flex flex-col gap-[10px] w-[350px] md:w-[650px] '>
+         <h2 className='text-[40px] md:text-[50px] lg:text-[70px]  text-white font-semibold  tracking-wide'>Why choose VeBook?</h2>
+         <p className='text-[14px] md:text-[18px] text-white text-justify font-normal tracking-wide '>Discover a wide range of sports venues at your fingertips with a hassle-free booking process, 
           flexible time slots to fit your schedule, and exclusive deals and discounts to enhance your experience</p>
           </div>
     </div>
 
     <div  className='container2'>
-    <div className='flex flex-col gap-[10px] w-[400px] md:w-[650px] '>
-         <h2 className='text-[30px] md:text-[50px] lg:text-[60px] text-white font-semibold  tracking-wide'>Find Venues Near You!</h2>
-         <p className='text-[18px] text-white text-justify font-semibold tracking-wide'>With VeBook, you can explore and book top-rated sports venues across Chennai city.
+    <div className='flex flex-col-reverse md:flex-row gap-[10px] w-[350px] md:w-[650px]  '>
+         <h2 className='text-[40px] md:text-[50px] lg:text-[70px] text-white font-semibold  tracking-wide'>Find venues near you!</h2>
+         <p className='text-[14px] md:text-[18px] text-white text-justify font-normal tracking-wide'>With VeBook, you can explore and book top-rated sports venues across Chennai city.
            Whether it’s a quick game with friends or a tournament, we’ve got you covered</p>
           </div>
-         <img src= {location} alt=""  className=' h-[300px] w-[600px] '/>
+         <img src= {location} alt=""  className=' h-[200px] w-[350px] md:h-[300px] md:w-[600px] '/>
     </div>
 
     </div>
